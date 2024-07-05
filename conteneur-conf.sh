a6a3b6287d384520c79cf22b33d3a61951e0df31
tutopalace <tutopalace@gmail.com>
2024-07-05 20:00:56 +0200

    Create conteneur-conf.sh

---
+++ conteneur-conf.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# MAJ
+apt update && apt -y full-upgrade && apt -y clean && apt -y autoremove
+
+# Installation paquets 
+apt install vim git sudo iproute2 net-tools cron 
+
+# Création user tutopalace
+useradd -m -s /bin/bash tutopalace
+
+# Ajout permission sudo pour tutopalace  
+echo "tutopalace ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/tutopalace
+
