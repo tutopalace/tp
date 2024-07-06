41d697464162e541389285ea59489db8bc0ee2a8
tutopalace <tutopalace@gmail.com>
2024-07-06 14:32:46 +0200

    Update conteneur-conf.sh

--- conteneur-conf.sh
+++ conteneur-conf.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Pour la configuration d'un serveur
-> 1. Mise à jour du serveur
-> 2. Installation des utilitaires nécessaires : vim git ...
-> 3. Ajout user 
-> 4. Configuration permissions sudo pour user
-> 5. Configuration SSH
-> 6. Crontab  ( maj programmée & reboot programmé )
-> 7. Configuration du pare-feu
-> 8. Configuration du serveur (Apache, gitlab, ...)
+## Pour la configuration d'un serveur
+## 1. Mise à jour du serveur
+## 2. Installation des utilitaires nécessaires : vim git ...
+## 3. Ajout user 
+## 4. Configuration permissions sudo pour user
+## 5. Configuration SSH
+## 6. Crontab  ( maj programmée & reboot programmé )
+## 7. Configuration du pare-feu
+## 8. Configuration du serveur (Apache, gitlab, ...)
 
 
 # 1. Mise à jour du serveur
