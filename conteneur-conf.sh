e122140fe918fa2b67966f4cdcab97b5219ef176
tutopalace <tutopalace@gmail.com>
2024-07-06 14:31:56 +0200

    Update conteneur-conf.sh etapes 2

--- conteneur-conf.sh
+++ conteneur-conf.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-
 # Pour la configuration d'un serveur
 > 1. Mise à jour du serveur
 > 2. Installation des utilitaires nécessaires : vim git ...
 > 3. Ajout user 
-> 4. configuration permissions sudo pour user
-  
-  
+> 4. Configuration permissions sudo pour user
+> 5. Configuration SSH
+> 6. Crontab  ( maj programmée & reboot programmé )
+> 7. Configuration du pare-feu
+> 8. Configuration du serveur (Apache, gitlab, ...)
+
 
 # 1. Mise à jour du serveur
 apt update && apt -y full-upgrade && apt -y clean && apt -y autoremove
