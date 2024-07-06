b0dff4242a7a84f8efb819a46961816f7ad6f2fb
tutopalace <tutopalace@gmail.com>
2024-07-06 14:27:33 +0200

    Update conteneur-conf.sh etapes conf

--- conteneur-conf.sh
+++ conteneur-conf.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-# MAJ
+
+# Pour la configuration d'un serveur
+> 1. Mise à jour du serveur
+> 2. Installation des utilitaires nécessaires : vim git ...
+> 3. Ajout user 
+> 4. configuration permissions sudo pour user
+  
+  
+
+# 1. Mise à jour du serveur
 apt update && apt -y full-upgrade && apt -y clean && apt -y autoremove
 
-# Installation paquets 
+# 2. Installation utilitaires (paquets) 
 apt install vim git sudo iproute2 net-tools cron ssh
 
 # Création user tutopalace
