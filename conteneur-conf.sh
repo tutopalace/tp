70617b733116a66a2534b15af4395121052f96f9
tutopalace <tutopalace@gmail.com>
2024-07-08 12:20:13 +0200

    Update conteneur-conf.sh

--- conteneur-conf.sh
+++ conteneur-conf.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+# Ce script devra permettre de configurer et de sécuriser un serveur. 
+# Ce script avance au rythme des videos disponibles sur la chaine @tutopalace
+# https://youtube.com/@tutopalace
+
+
 ## Pour la configuration d'un serveur
 ## 1. Mise à jour du serveur
 ## 2. Installation des utilitaires nécessaires : vim git ...
 ## 3. Ajout user 
 ## 4. Configuration permissions sudo pour user
-## 5. Configuration SSH
-## 6. Crontab  ( maj programmée & reboot programmé )
-## 7. Configuration du pare-feu
-## 8. Configuration du serveur (Apache, gitlab, ...)
+
+## A venir ...
+## 5. Crontab  ( maj programmée & reboot programmé )
+## 6. Configuration SSH
+## ...
+
 
 
 # 1. Mise à jour du serveur
