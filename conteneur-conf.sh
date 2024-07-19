362e15cb894a2c5c02992d75b6e6a9b462506581
tutopalace <tutopalace@gmail.com>
2024-07-19 20:46:28 +0200

    Update conteneur-conf.sh

--- conteneur-conf.sh
+++ conteneur-conf.sh
@@ -23,7 +23,7 @@
 apt update && apt -y full-upgrade && apt -y clean && apt -y autoremove
 
 # 2. Installation utilitaires (paquets) 
-apt install vim git sudo iproute2 net-tools cron ssh
+apt install vim git sudo iproute2 net-tools iputils-ping cron ssh
 
 # 3. Création user tutopalace
 useradd -m -s /bin/bash tutopalace
